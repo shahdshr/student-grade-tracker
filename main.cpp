eba46ca45ef1eb12093f76df42378ae1fd48d64a
Shahd <chahdsahraoui04@gmail.com>
2026-09-11 20:51:51 +0100

    new feature

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <string>
 
 int main() {
     std::cout << "Student Grade Tracker\n";
-    std::cout<<"hello git \n";
+    std::cout << "hello git\n";
+
+    std::string name;
+
+    std::cout << "Enter student name: ";
+    std::cin >> name;
+
+    std::cout << "Hello, " << name << "!\n";
+
     return 0;
 }
